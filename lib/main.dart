523ad4fcc0c3b57d35f07b3ea431b8fdad0ef560
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-09 12:01:16 -0600

    updt main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:proyecto_cine_flutter/config/router/app_router.dart';
 import 'package:proyecto_cine_flutter/config/theme/app_theme.dart';
 
@@ -7,7 +8,7 @@ Future<void> main() async{
   await dotenv.load(fileName: '.env');
 
   runApp(
-    const ProvideScope(child: MainApp())
+    const ProviderScope(child: MainApp())
   );
 }
 
