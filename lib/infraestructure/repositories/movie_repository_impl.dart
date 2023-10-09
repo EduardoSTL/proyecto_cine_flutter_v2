62a19599fd4cd41d30cf9bea5e3e6d57fa3bae61
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-09 11:47:37 -0600

    added: movie repository

---
+++ lib/infraestructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'package:proyecto_cine_flutter/domain/datasource/movie_datasource.dart';
+import 'package:proyecto_cine_flutter/domain/entities/movie.dart';
+import 'package:proyecto_cine_flutter/domain/repositories/movies_repositories';
+
+class MovieRepositoryImpl extends MoviesRepositories{
+  final MovieDataSource datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying ({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+  @override
+  Future<List<Movie>> getPopular ({int page = 1}) {
+    return datasource.getPopular(page: page);
+  }
+
+  @override
+  Future<List<Movie>> getTopRated ({int page = 1}) {
+    return datasource.getTopRated(page: page);
+  }
+
+  @override
+  Future<List<Movie>> getUpcoming ({int page = 1}) {
+    return datasource.getUpcoming(page: page);
+  }
+}
