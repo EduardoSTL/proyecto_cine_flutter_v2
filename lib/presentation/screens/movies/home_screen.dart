8d0c04fc84c169658f4b8b0d1f586af7131c6a7b
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-12 11:54:04 -0600

    updt home screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:proyecto_cine_flutter/presentation/providers/movies/movies_provider.dart';
+import 'package:proyecto_cine_flutter/presentation/providers/movies/initial_loading_provider.dart';
+import 'package:proyecto_cine_flutter/presentation/providers/movies/movies_providers.dart';
+import 'package:proyecto_cine_flutter/presentation/providers/movies/movies_sideshow_provider.dart';
+import 'package:proyecto_cine_flutter/presentation/widgets/movies/movie_horizontal_listview.dart';
+import 'package:proyecto_cine_flutter/presentation/widgets/movies/movies_sideshow.dart';
+import 'package:proyecto_cine_flutter/presentation/widgets/shared/custom_appbar.dart';
+import 'package:proyecto_cine_flutter/presentation/widgets/shared/custom_bottom_navigation.dart';
+import 'package:proyecto_cine_flutter/presentation/widgets/shared/full_screen_loader.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -17,7 +24,7 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget{
+class _HomeView extends ConsumerStatefulWidget {
   const _HomeView();
 
   @override
@@ -27,12 +34,84 @@ class _HomeView extends ConsumerStatefulWidget{
 class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    ref.read( popularMoviesProvider.notifier ).loadNextPage();
+    ref.read( topRatedMoviesProvider.notifier ).loadNextPage();
+    ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if ( initialLoading ) return const FullScreenLoader();
+    
+    final slideShowMovies = ref.watch( moviesSlideShowProvider );
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final popularMovies = ref.watch( popularMoviesProvider );
+    final topRatedMovies = ref.watch( topRatedMoviesProvider );
+    final upcomingMovies = ref.watch( upcomingMoviesProvider );
+
+    return CustomScrollView(
+      slivers: [
+
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppBar(),
+          ),
+        ),
+
+
+        SliverList(delegate: SliverChildBuilderDelegate(
+          (context, index) {
+              return Column(
+                  children: [
+              
+                    // const CustomAppbar(),
+              
+                    MoviesSlideshow(movies: slideShowMovies ),
+              
+                    MovieHorizontalListview(
+                      movies: nowPlayingMovies,
+                      title: 'En cines',
+                      subTitle: 'Lunes 20',
+                      loadNextPage: () =>ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
+                      
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: upcomingMovies,
+                      title: 'Próximamente',
+                      subTitle: 'En este mes',
+                      loadNextPage: () =>ref.read(upcomingMoviesProvider.notifier).loadNextPage()
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: popularMovies,
+                      title: 'Populares',
+                      // subTitle: '',
+                      loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
+                    ),
+              
+                    MovieHorizontalListview(
+                      movies: topRatedMovies,
+                      title: 'Mejor calificadas',
+                      subTitle: 'Desde siempre',
+                      loadNextPage: () =>ref.read(topRatedMoviesProvider.notifier).loadNextPage()
+                    ),
+
+                    const SizedBox( height: 10 ),
+              ],
+            );
+          },
+          childCount: 1
+        )),
+      ]
+    );
   }
 }
