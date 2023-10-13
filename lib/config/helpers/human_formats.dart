8e9c748c2e73d6064205690c3efeb17f246307b2
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-13 08:40:24 -0600

    updt

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -1,11 +1,12 @@
 import 'package:intl/intl.dart';
 
-//formato para los decimales
+//formato para los decimales (en toda la app)
 class HumanFormats{
   static String number (double number){
     final formattedNumber = NumberFormat.compactCurrency(
       decimalDigits: 0,
       symbol: '',
+      //tipo de moneda(USA):
       locale: 'en', 
     ).format(number);
     return formattedNumber;
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:proyecto_cine_flutter/presentation/screens/movies/home_screen.dart';
 
+//definimos archivo de apertura
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
